0fd2b5bf32118052b6a9485c0dad56f052c8d9e1
TimFB <56914732+TibiIius@users.noreply.github.com>
2020-11-28 14:29:57 +0100

    added mariadb

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
-const express = require("express");
+import express from "express";
+import mariadb from "mariadb";
 import { Server } from "typescript-rest";
 import { ProductService } from "./product";
 import { StoreService } from "./store";
@@ -6,6 +7,12 @@ import { UserService } from "./user";
 
 const app = express();
 Server.buildServices(app, ProductService, StoreService, UserService);
+const pool =  mariadb.createPool({
+  host: 'localhost', 
+  user:'root', 
+  password: 'myPassword',
+  connectionLimit: 5
+});
 
 app.listen(8000, function () {
   console.log("Rest Server listening on port 8000!");
