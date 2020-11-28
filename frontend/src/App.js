a8f4c0d5b470d20dd52b16da4dbe8b26f8f7a859
C.Scheoning <schoeningchristopher@gmail.com>
2020-11-28 11:39:28 +0100

    routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,12 +6,21 @@ import { Login } from './routes/login/Login';
 import './App.scss';
 import { Hero } from './routes/hero/Hero';
 
+<<<<<<< HEAD
+=======
+export const FourOFour = () => <p>404 - Page not found 🐱‍👤</p>;
+
+>>>>>>> 002ec46... routing
 export const App = () => (
 	<BrowserRouter>
 		<Switch>
 			<Route path='/login' component={Login} />
+<<<<<<< HEAD
 			<Route exact path='/' component={Hero} />
 			<Route component={() => <p>404 - Page not found 🐱‍👤</p>} />
+=======
+			<Route component={FourOFour} />
+>>>>>>> 002ec46... routing
 		</Switch>
 	</BrowserRouter>
 );
