d74d8f23396ace0d006128ad8863007da1d4e383
C.Scheoning <schoeningchristopher@gmail.com>
2020-11-28 15:05:21 +0100

    loginpage

--- frontend/src/routes/login/components/LoginForm.js
+++ frontend/src/routes/login/components/LoginForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
@@ -8,20 +8,74 @@ import Switch from '@material-ui/core/Switch';
 
 import './LoginForm.scss';
 
-export const LoginForm = () => (
-	<div data-testid='loginForm' className='loginForm'>
-		<Card>
+export const LoginForm = ({ initialFormState = 'login' }) => {
+	const [formState, setFormState] = useState(initialFormState);
+	const [isLookingToSell, setIsLookingToSell] = useState(false);
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [license, setLicense] = useState('');
+
+	return (
+		<Card data-testid='loginForm' className='loginForm'>
+			<div className='loginSignupToggleWrapper'>
+				<p
+					className={formState === 'login' ? '' : 'dimmed'}
+					onClick={() => {
+						setFormState('login');
+						setIsLookingToSell(false);
+					}}
+				>
+					Login
+				</p>
+				<p className='dimmed'>|</p>
+				<p
+					className={formState === 'signup' ? '' : 'dimmed'}
+					onClick={() => setFormState('signup')}
+				>
+					Signup
+				</p>
+			</div>
+
 			<CardContent className='cardContent'>
 				<form className='form' noValidate autoComplete='off'>
-					<TextField label='Username' variant='outlined' />
-					<TextField label='Password' variant='outlined' />
-					<div>
-						<br />
-						<p>Looking to sell?</p>
-						<Switch />
-						<TextField label='License' variant='outlined' />
-					</div>
+					<TextField
+						label='Username'
+						variant='outlined'
+						value={username}
+						onChange={(e) => setUsername(e.target.value)}
+					/>
+					<br />
+					<TextField
+						label='Password'
+						variant='outlined'
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+					{formState === 'signup' && (
+						<>
+							<br />
+							<br />
+							<div className='sellToggleWrapper'>
+								<p>Looking to sell?</p>
+								<Switch
+									value={isLookingToSell}
+									onChange={(e) => {
+										setIsLookingToSell(e.target.checked);
+									}}
+								/>
+							</div>
+							{isLookingToSell && (
+								<TextField
+									label='License'
+									variant='outlined'
+									value={license}
+									onChange={(e) => setLicense(e.target.value)}
+								/>
+							)}
+						</>
+					)}
 				</form>
+				<br />
 			</CardContent>
 			<CardActions className='actions'>
 				<Button color='primary' variant='contained' type='submit'>
@@ -29,5 +83,5 @@ export const LoginForm = () => (
 				</Button>
 			</CardActions>
 		</Card>
-	</div>
-);
+	);
+};
