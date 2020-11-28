002ec465bf4d5876d9176f1b2e6023e0da7aeb9e
C.Scheoning <schoeningchristopher@gmail.com>
2020-11-28 11:39:28 +0100

    routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import { Login } from './routes/login/Login';
+
 import './App.scss';
 
-export const App = () => <p>GoLocal</p>;
+export const FourOFour = () => <p>404 - Page not found 🐱‍👤</p>;
+
+export const App = () => (
+	<BrowserRouter>
+		<Switch>
+			<Route path='/login' component={Login} />
+			<Route component={FourOFour} />
+		</Switch>
+	</BrowserRouter>
+);
