aad62e3b0c170de0fade0016d4ec4fb73f447230
C.Scheoning <schoeningchristopher@gmail.com>
2020-11-29 16:07:54 +0100

    reverts to backend

--- backend/src/user.ts
+++ backend/src/user.ts
@@ -1,121 +1,121 @@
 import {
-	Path,
-	PathParam,
-	QueryParam,
-	GET,
-	POST,
-	PUT,
-	Context,
-	ServiceContext,
-	Errors,
-	Return,
-	ContextRequest,
-} from 'typescript-rest';
-import { ProductAndAmount } from './product';
-import express from 'express';
+  Path,
+  PathParam,
+  QueryParam,
+  GET,
+  POST,
+  PUT,
+  Context,
+  ServiceContext,
+  Errors,
+  Return,
+  ContextRequest,
+} from "typescript-rest";
+import { ProductAndAmount } from "./product";
+import express from "express";
 
 interface User {
-	id: number;
-	username: string;
-	passwordHash: string;
-	firstName: string;
-	lastName: string;
-	email: string;
-	phone: string;
-	longitude: number;
-	latitude: number;
-	isFarmer: boolean;
-	products?: ProductAndAmount[];
-	farmingMethodology?: string;
-	covidGuidelines?: string;
+  id: number;
+  username: string;
+  passwordHash: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  longitude: number;
+  latitude: number;
+  isFarmer: boolean;
+  products?: ProductAndAmount[];
+  farmingMethodology?: string;
+  covidGuidelines?: string;
 }
 
 interface Login {
-	username: string;
-	password: string;
+  username: string;
+  password: string;
 }
 
-@Path('/user')
+@Path("/user")
 export class UserService {
-	@Context
-	context: ServiceContext;
+  @Context
+  context: ServiceContext;
 
-	@POST
-	createUser(
-		createdUser: User,
-		@ContextRequest request: express.Request
-	): Return.NewResource<void> {
-		return new Return.NewResource<void>(request.url + '/' + createdUser.id);
-	}
+  @POST
+  createUser(
+    createdUser: User,
+    @ContextRequest request: express.Request
+  ): Return.NewResource<void> {
+    return new Return.NewResource<void>(request.url + "/" + createdUser.id);
+  }
 
-	@Path('/login')
-	@POST
-	loginUser(login: Login): User {
-		// Check if the following parameters match the database entries
-		// login.username && login.password
-		// true -> return '{"status":"ok"}';
-		// false -> return '{"status":"err"}';
-		// Placeholder until database is set up
-		let user: User = {
-			id: 0,
-			username: 'admin',
-			passwordHash: 'abcd123',
-			firstName: 'admin',
-			lastName: 'admin',
-			email: 'admin@example.org',
-			phone: '+490000',
-			longitude: 0,
-			latitude: 0,
-			isFarmer: false,
-		};
-		return user;
-	}
+  @Path("/login")
+  @POST
+  loginUser(login: Login): User {
+    // Check if the following parameters match the database entries
+    // login.username && login.password
+    // true -> return '{"status":"ok"}';
+    // false -> return '{"status":"err"}';
+    // Placeholder until database is set up
+    let user: User = {
+      id: 0,
+      username: "admin",
+      passwordHash: "abcd123",
+      firstName: "admin",
+      lastName: "admin",
+      email: "admin@example.org",
+      phone: "+490000",
+      longitude: 0,
+      latitude: 0,
+      isFarmer: false,
+    };
+    return user;
+  }
 
-	@Path('/logout')
-	@GET
-	logoutUser(@QueryParam('username') username: string): void {}
+  @Path("/logout")
+  @GET
+  logoutUser(@QueryParam("username") username: string): void {}
 
-	@Path(':username')
-	@GET
-	getUserInfo(@PathParam('username') username: string): User {
-		// Placeholder until database is set up
-		try {
-			let user: User = {
-				id: 0,
-				username: username,
-				passwordHash: 'abcd123',
-				firstName: 'admin',
-				lastName: 'admin',
-				email: 'admin@example.org',
-				phone: '+490000',
-				longitude: 0,
-				latitude: 0,
-				isFarmer: false,
-			};
-			return user;
-		} catch (error) {
-			throw new Errors.BadRequestError();
-		}
-	}
+  @Path(":username")
+  @GET
+  getUserInfo(@PathParam("username") username: string): User {
+    // Placeholder until database is set up
+    try {
+      let user: User = {
+        id: 0,
+        username: username,
+        passwordHash: "abcd123",
+        firstName: "admin",
+        lastName: "admin",
+        email: "admin@example.org",
+        phone: "+490000",
+        longitude: 0,
+        latitude: 0,
+        isFarmer: false,
+      };
+      return user;
+    } catch (error) {
+      throw new Errors.BadRequestError();
+    }
+  }
 
-	@PUT
-	editUserInfo(editedUser: User): User {
-		try {
-			let user: User = {
-				id: 0,
-				username: editedUser.username,
-				passwordHash: 'abcd123',
-				firstName: 'admin',
-				lastName: 'admin',
-				email: 'admin@example.org',
-				phone: '+490000',
-				longitude: 0,
-				latitude: 0,
-				isFarmer: false,
-			};
-			return user;
-		} catch (error) {
-			throw new Errors.BadRequestError();
-		}
-	}
+  @PUT
+  editUserInfo(editedUser: User): User {
+    try {
+      let user: User = {
+        id: 0,
+        username: editedUser.username,
+        passwordHash: "abcd123",
+        firstName: "admin",
+        lastName: "admin",
+        email: "admin@example.org",
+        phone: "+490000",
+        longitude: 0,
+        latitude: 0,
+        isFarmer: false,
+      };
+      return user;
+    } catch (error) {
+      throw new Errors.BadRequestError();
+    }
+  }
 }
