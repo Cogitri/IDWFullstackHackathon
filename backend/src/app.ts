264416c903073cab8e43af9b05d60007eab02c67
C.Scheoning <schoeningchristopher@gmail.com>
2020-11-29 16:07:54 +0100

    reverts to backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,19 +1,19 @@
-import express from 'express';
-import mariadb from 'mariadb';
-import { Server } from 'typescript-rest';
-import { ProductService } from './product';
-import { StoreService } from './store';
-import { UserService } from './user';
+import express from "express";
+import mariadb from "mariadb";
+import { Server } from "typescript-rest";
+import { ProductService } from "./product";
+import { StoreService } from "./store";
+import { UserService } from "./user";
 
 const app = express();
 Server.buildServices(app, ProductService, StoreService, UserService);
 const pool = mariadb.createPool({
-	host: 'localhost',
-	user: 'root',
-	password: 'myPassword',
-	connectionLimit: 5,
+  host: "localhost",
+  user: "root",
+  password: "myPassword",
+  connectionLimit: 5,
 });
 
 app.listen(8000, function () {
-	console.log('Rest Server listening on port 8000!');
+  console.log("Rest Server listening on port 8000!");
 });
