67660445e1a737d057058f4b1b00630c9d0d21fd
Rasmus Thomsen <oss@cogitri.dev>
2020-11-30 14:13:19 +0100

    backend: fix tests

--- backend/src/__tests__/store.ts
+++ backend/src/__tests__/store.ts
@@ -45,31 +45,6 @@ describe("POST /product", () => {
           }
           done();
         });
-      request(app)
-        .post("/product")
-        .send({
-          product: {
-            category: { name: "test2", id: 2 },
-            deliveryMethod: "bike",
-            description: "much wow",
-            expiryDate: "2020-11-29",
-            manufacturingDate: "2020-11-28",
-            paymentMethod: "paypal",
-            price: 15,
-            name: "carrot",
-            status: "available",
-          },
-          amount: 15,
-          farmerId: 1,
-        })
-        .expect(201)
-        .end(function (err, res) {
-          if (err) {
-            console.log(res.text);
-            throw err;
-          }
-          done();
-        });
     });
   });
 });
